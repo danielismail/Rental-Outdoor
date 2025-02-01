f178739f994f5f9fa76bcef825ef3f41af713cb1
barronbrn <qwebaron7@gmail.com>
2025-02-01 12:07:44 +0700

    Card Produk

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Peralatan;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeder\PeralatanSeeder;
+use Database\Seeders\PeralatanSeeder as SeedersPeralatanSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call(SeedersPeralatanSeeder::class);
 
         User::factory()->create([
             'name' => 'Test User',
